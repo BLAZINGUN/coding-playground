f0f6dd60103323f87b6817a8a6a949666f3dfdd0
Srikantam Madhava Rao <madhavsai36@gmail.com>
2023-08-27 00:14:45 +0530

    testbenches practice

--- system verilog/threads/fork_join_none_using_fork_join.sv
+++ system verilog/threads/fork_join_none_using_fork_join.sv
@@ -1,20 +1,24 @@
-//fork join_none functionality using fork join
-
 module top;
+ 
+event e1,e2;
 
 initial 
   begin
       fork
           begin : b1
             #5 $display($time, " from block b1");   
+            -> e1;
           end : b1
-
+ 
           begin : b2
             #10 $display($time, " from block b2");
+            -> e2;
           end : b2
       join
-
+    
+    wait(e1.triggered || e2.triggered);
     $display($time, " from outside fork-join block");
   end
-
+ 
 endmodule
+
